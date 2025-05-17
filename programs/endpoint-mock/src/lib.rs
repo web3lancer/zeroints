871fac2f6e92eba9778d6eb4432ac3c87f949fd9
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-05-17 06:33:29 +0100

    🌍 feat: Update OFT address in Anchor.toml and lib.rs, add docx to .gitignore

--- programs/endpoint-mock/src/lib.rs
+++ programs/endpoint-mock/src/lib.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 use instructions::*;
 use state::*;
 
-declare_id!("6xmPjYnXyxz36xcKkv2zCrZc72LK5hQ9xzY3EjeZ59MV");
+declare_id!("J4YwD9TBbyV84SECi7Zukn7CHrjX8HQ2qrr5Xx2fBvum");
 
 pub const OAPP_SEED: &[u8] = b"OApp";
 
